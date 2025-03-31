77fdc95d8c4d0d16b3d49810179ec8959759ca07
Navya Singh <singhn13@msu.edu>
2025-03-30 23:59:55 -0400

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const FoodList = ({ foods }) => (
 );
 
 const StatusMessage = ({ count }) => {
-  return <p>{count > 5 ? 'You really love food' : 'Keep discovering new dishes'}</p>;
+  return <p>{count > 2 ? 'You really love food!' : 'Keep discovering new dishes'}</p>;
 };
 
 function App() {
@@ -27,29 +27,16 @@ function App() {
   ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
+    <>      
       <Hello />
       <FoodList foods={foods} />
       <StatusMessage count={count} />
       
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Dishes count is {count}
         </button>
       </div>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
